cb43e75ab635db6fbf1aec7a43a3fcbde4bda7af
Crowdbotics <team@crowdbotics.com>
2023-12-13 21:49:47 +0000

    Initial commit

---
+++ backend/doxo_demo_44999/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for doxo_demo_44999 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "doxo_demo_44999.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "doxo-demo-44999",
+  url: "https://doxo-demo-44999.botics.co",
+  api: "https://doxo-demo-44999.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
